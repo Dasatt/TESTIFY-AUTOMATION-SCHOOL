fe76de1e35fba4eaf253a567d435e9456139ebfc
Dasat <satopeoladayo@gmail.com>
2023-06-26 00:08:46 +0100

    added all javascript work done so far

---
+++ Module 3 - Javascript/Module 3A/app.js
@@ -0,0 +1,22 @@
+// const day = 'Monday'
+// const position = 2
+
+// const output = 'Today is ' + day + '. It is the ' + position + 'nd day of the week.'
+
+// console.log(output)
+
+// const copare =
+
+/* 
+AND --> && all must be true  
+OR --> || either must be trye
+NOT --> ! toggles boolean values
+*/
+
+const age = 15
+
+if (age>=18){
+    console.log("You are eligible to vote")
+} else{
+    console.log("You are not eligible to vote")
+}
