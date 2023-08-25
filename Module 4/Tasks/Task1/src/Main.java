4b176539e71e62ca1bc57c339958379d3498a63c
Dasat <satopeoladayo@gmail.com>
2023-08-25 03:49:07 +0100

    Module 4 Task Folder

--- Module 4/Tasks/Task1/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Welcome to Testify Academy");
-        System.out.println("Welcome to Test Automation School");
-        System.out.println("Welcome to Module 4");
-        System.out.println("Welcome to the Module on Java");
-    }
-}
