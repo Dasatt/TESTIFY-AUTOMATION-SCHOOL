8e26fcac3e0b7d2d7722c10a9141e51d794a4017
Dasat <satopeoladayo@gmail.com>
2023-08-25 02:22:59 +0100

    Completed MOdule 4 Task 1

---
+++ Module 4/Tasks/Task1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Welcome to Test Automation School");
+        System.out.println("Welcome to Module 4");
+        System.out.println("Welcome to the Module on Java");
+    }
+}
