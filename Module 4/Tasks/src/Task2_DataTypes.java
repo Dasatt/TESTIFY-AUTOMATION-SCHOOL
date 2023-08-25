56d87fd64947d54180c0df9af1ff21dac6673414
Dasat <satopeoladayo@gmail.com>
2023-08-25 03:53:24 +0100

    Task 2- Data Types

---
+++ Module 4/Tasks/src/Task2_DataTypes.java
@@ -0,0 +1,31 @@
+public class Task2_DataTypes {
+    public static void main(String[] args) {
+        /*bytes -128 to 128
+        short -32,768 to 32,767
+        int -2,147,483,648 to 2,147,483,647
+        long -9,223,372,036,854,775,808 to 9,223,32,036,854,775,807
+        float -6 to 7 decimal digits
+        double float from 15 decimal digits
+        char single letters = 'a' 'b'
+        boolean true or false*/
+
+        byte age = 127;
+        short money = 32767;
+        int tooMuchMoney = 2147483647;
+        long longerMoney = 922332036854775807l;
+        float area = 21.474845f;
+        double biggerArea = 1234.5344588888;
+//        char option = 'n';
+//        boolean isAround = true;
+
+        System.out.println(age);
+        System.out.println(money);
+        System.out.println(tooMuchMoney);
+        System.out.println(longerMoney);
+        System.out.println(area);
+        System.out.println(biggerArea);
+//        System.out.println(option);
+//        System.out.println(isAround);
+
+    }
+}
