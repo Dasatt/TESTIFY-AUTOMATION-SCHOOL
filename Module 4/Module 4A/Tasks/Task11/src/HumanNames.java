d8b2c9d7e428c7f240879a89b524c639e9d3d132
Dasat <satopeoladayo@gmail.com>
2023-09-25 12:51:04 +0100

    One week challenge & Tasks recommited due to folder restructuring

---
+++ Module 4/Module 4A/Tasks/Task11/src/HumanNames.java
@@ -0,0 +1,5 @@
+public class HumanNames {
+    public String firstName(String name){
+        return name;
+    }
+}
---
+++ Module 4/Module 4A/Tasks/Task17A/src/userLogin.java
@@ -0,0 +1,19 @@
+public class userLogin extends LoginFramework {
+
+    @Override
+    public void signIn() {
+        System.out.println("Signin");
+
+    }
+
+    @Override
+    public void continueToHomepage() {
+        System.out.println("Continue to new page");
+
+    }
+
+    @Override
+    public void OAUth() {
+
+    }
+}
