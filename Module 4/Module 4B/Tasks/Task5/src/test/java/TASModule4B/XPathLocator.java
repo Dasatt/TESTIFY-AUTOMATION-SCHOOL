4bd1966d3833825882446df6cdc0b7262f8e0b42
Dasat <satopeoladayo@gmail.com>
2023-10-15 01:01:11 +0100

    Task 5 Xpath locator

--- Module 4/Module 4B/Tasks/Task5/src/test/java/TASModule4B/XPathLocator.java
+++ Module 4/Module 4B/Tasks/Task5/src/test/java/TASModule4B/XPathLocator.java
@@ -1,7 +1,10 @@
 package TASModule4B;
-/*  Navigate to http://demo.guru99.com/
-    Click on the security Project menu.
-    Input any text in the email and password field.*/
+/*  Navigate to https://www.saucedemo.com/
+Login using username and password.
+Click on the login button,
+click on the "add to cart" button of any product (Add only one item)
+click the cart icon on the top right.
+Click on checkout.  USE ONLY XPATH LOCATION STRATEGY.*/
 
 
 import org.openqa.selenium.By;
@@ -10,7 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class XPathLocator {
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\YEMISI\\OneDrive\\Documents\\Software Testing\\Testify Academy\\Testify Automation School Repo\\Module 4\\Module 4B\\Tasks\\Task4\\src\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\YEMISI\\OneDrive\\Documents\\Software Testing\\Testify Academy\\Testify Automation School Repo\\Module 4\\Module 4B\\Tasks\\Task5\\src\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://saucedemo.com");
         driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
